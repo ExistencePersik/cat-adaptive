8f7c2144cd36b3e421432b8f1c59fb54c3dc4f7e
Existence Persik <prostor130@gmail.com>
2022-02-05 01:37:23 +0700

    automation + optimization

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,40 @@ const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const ghPages = require('gulp-gh-pages');
+
+// Images
+
+const images = () => {
+  return gulp.src("source/img/**/*.{jpg,png,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.mozjpeg({quality: 95, progressive: true}),
+    imagemin.svgo()
+    ]))
+ }
+
+exports.images = images;
+
+// Server
+
+const server = (done) => {
+  sync.init({
+    server: {
+      baseDir: 'build'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
+}
+
+exports.server = server;
 
 // Styles
 
@@ -16,28 +50,39 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
 
-// Server
+// Copy
 
-const server = (done) => {
-  sync.init({
-    server: {
-      baseDir: 'source'
-    },
-    cors: true,
-    notify: false,
-    ui: false,
-  });
-  done();
-}
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico",
+    "source/*.html"
+  ], {
+  base: "source"
+  })
+ .pipe(gulp.dest("build"));
+};
 
-exports.server = server;
+exports.copy = copy;
+
+// Clean
+
+const clean = () => {
+  return del("build");
+};
+
+exports.clean = clean;
 
 // Watcher
 
@@ -49,3 +94,21 @@ const watcher = () => {
 exports.default = gulp.series(
   styles, server, watcher
 );
+
+// Build
+
+var build = gulp.series(
+  clean,
+  copy,
+  styles,
+  images
+);
+
+exports.build = build;
+
+// GHP
+
+gulp.task('deploy', function() {
+  return gulp.src('./build/**/*')
+    .pipe(ghPages());
+});
